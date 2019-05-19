a56e32b20db145e578b472c72c6114be987d1dc6
Lucas Amaral <diegolucasb@gmail.com>
2019-05-19 14:54:55 -0300

    :tada: First commit

---
+++ app/src/main/java/dev/lucasdabs/carselection/MainActivity.kt
@@ -0,0 +1,12 @@
+git inipackage dev.lucasdabs.carselection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
